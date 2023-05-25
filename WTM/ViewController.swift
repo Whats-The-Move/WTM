e7459d951350196d3a5b7807eec1d19cc9b0605b
Nikunj Tyagi <nikunjt0@gmail.com>
2023-05-25 13:22:32 -0500

    four tabs

--- WTM/ViewController.swift
+++ WTM/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var emailInvalid: UILabel!
     
-    @IBOutlet weak var fratButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -153,7 +152,8 @@ class ViewController: UIViewController {
                 usersCollection.document(uid!).setData([
                     "email": email,
                     "uid": uid!,
-                    "images": []
+                    "images": [],
+                    "username": username
                 ]) { (error) in
                     if let error = error {
                         print("Error adding document: \(error)")
@@ -213,15 +213,6 @@ class ViewController: UIViewController {
         password.isSecureTextEntry = true
         password.frame = CGRect(x: 20, y: email.frame.origin.y + email.frame.size.height + 10, width: view.frame.size.width - 100, height: 50)
         button.frame = CGRect(x: -70 + view.frame.size.width, y: password.frame.origin.y, width: 50, height: 50)
-        fratButton.frame = CGRect(x: 20,
-                                  y: label.frame.origin.y + label.frame.size.height + 180,
-                                  width: view.frame.size.width - 40,
-                                  height: 50)
-        fratButton.contentHorizontalAlignment = .center
-        fratButton.layer.borderWidth = 4
-        fratButton.layer.borderColor = UIColor.black.cgColor
-        fratButton.layer.cornerRadius = 10
-        fratButton.clipsToBounds = true
 
 
 
