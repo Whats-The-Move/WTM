353296439e9610b56fa5c37e03ef6f88a8ec2439
Nikunj Tyagi <nikunjt0@gmail.com>
2023-08-15 22:34:58 -0500

    checking for correct authentication

--- WTM/LoadingPageViewController.swift
+++ WTM/LoadingPageViewController.swift
@@ -1,23 +1,32 @@
-//
-//  LoadingPageViewController.swift
-//  WTM
-//
-//  Created by Nikunj  Tyagi on 1/21/23.
-//
-
 import UIKit
+import FirebaseAuth
 
 class LoadingPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if launchedBefore && authenticated {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let appHomeVC = storyboard.instantiateViewController(identifier: "TabBarController")
-            appHomeVC.modalPresentationStyle = .overFullScreen
-            self.present(appHomeVC, animated: true)
+        
+        // Check if the user is authenticated in Firebase
+        if let currentUser = Auth.auth().currentUser {
+            // User is authenticated in Firebase, check "authenticated" flag
+            if authenticated && launchedBefore {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let appHomeVC = storyboard.instantiateViewController(identifier: "TabBarController")
+                appHomeVC.modalPresentationStyle = .overFullScreen
+                self.present(appHomeVC, animated: true)
+            } else {
+                UserDefaults.standard.set(true, forKey: "launchedBefore")
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateViewController(identifier: "SignUpPage")
+                vc.modalPresentationStyle = .overFullScreen
+                self.present(vc, animated: true)
+            }
         } else {
-            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            // User is not authenticated in Firebase, proceed based on "authenticated" flag
+            if authenticated {
+                UserDefaults.standard.set(false, forKey: "authenticated")
+            }
+            
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let vc = storyboard.instantiateViewController(identifier: "SignUpPage")
             vc.modalPresentationStyle = .overFullScreen
